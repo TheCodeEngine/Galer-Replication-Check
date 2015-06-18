9b41f57468729fa46c1eed084b3a96234bc9d5fb
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2015-06-18 22:04:31 +0200

    table: remove not used table code

--- pkg/table.py
+++ pkg/table.py
@@ -43,26 +43,4 @@ class ClusterTable(Table):
 		table = self.renderTable(nodes, wsrep_vars, fx=fx, fy=fy)
 		table.align["var"] = "l"
 
-
-
-		#table = PrettyTable(['Cluster Intigrity Var'] + nodes + ['Check'])
-		#table.align['Cluster Intigrity Var'] = "l"
-#
-		#for status_key, status_value in wsrep_vars.items():
-		#	for key, value in status_value.items():
-		#		attr_list = self.__get_list_with_vars(key, nodes)
-		#		status = self.__statusStringListEqual(attr_list) if status_key == 'cluster-integrity' else ''
-		#		table.add_row([key] + attr_list + [status])
-		#	table.add_row([''] * (self.cluster.count() + 2))
-
 		return table
-
-	def __statusStringListEqual(self, attr_list):						# @todo deprecated
-		ok_style = click.style('OK', fg='green')
-		error_style = click.style('Error', bold=True, fg='red')
-
-		return ok_style if self.listEqual(attr_list) else error_style
-
-	def __get_list_with_vars(self, wsrep_var, nodes):					# todo @deprecated
-		attr_list = map(lambda x: x.getvar(wsrep_var), nodes)
-		return attr_list
