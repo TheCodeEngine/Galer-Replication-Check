018de2f7ef4f8716b2ad834f6574d2b1b87c1010
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2015-06-18 16:56:46 +0200

    galer-cli: add GaleraCLI class to command line tool

--- pkg/cli.py
+++ pkg/cli.py
@@ -1,5 +1,20 @@
+import click
+from pkg.database import Cluster
+from pkg.table import ClusterTable
+
 
 class AppInfo:
 	@staticmethod
 	def version_info():
 		return 'Version 1.2.1 Copyright TheCodeEngine\nUnder MIT License http://opensource.org/licenses/MIT'
+
+
+class GaleraCLI:
+	@staticmethod
+	def check(user, password, hosts):
+		click.echo('\n+--- Checking Cluster Intigrity:')
+		cluster = Cluster(nodes=hosts, user=user, password=password)
+		with click.progressbar(length=cluster.count(), label='Fetching Data') as bar:
+			cluster.fetch(bar.update)
+		table = ClusterTable(cluster)
+		table.render()
