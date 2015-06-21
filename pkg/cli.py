544fd0b35bdae751efc28fd8cd868fa2395116f7
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2015-06-21 14:36:19 +0200

    update version tag

--- pkg/cli.py
+++ pkg/cli.py
@@ -7,7 +7,7 @@ from pkg.config import Config
 class AppInfo:
 	@staticmethod
 	def version_info():
-		return 'Version 1.2.1 Copyright TheCodeEngine\nUnder MIT License http://opensource.org/licenses/MIT'
+		return 'Version 1.3.0 Copyright TheCodeEngine\nUnder MIT License http://opensource.org/licenses/MIT'
 
 
 class GaleraCLI:
