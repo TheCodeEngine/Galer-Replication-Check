8f6c77337a3e471580e42ef8ce6b123650803ab8
Konstantin Stoldt <konstantin.stoldt@talk-point.de>
2015-06-21 11:33:57 +0200

    config: implement search

--- pkg/config.py
+++ pkg/config.py
@@ -27,5 +27,36 @@ class Config(object):
         :return:
         """
         exists = filter(lambda x: file.can_access(x), self.config_files)
-        self.config = [self.cfg.read(z) for z in exists]
+        self.config = [z for z in exists]
         return self.config
+
+    def search(self, section, varname):
+        """
+        search a variable in the config files and return the value
+        :param varname:
+        :return:
+        """
+        return [self.__exists_in_config(z, section, varname) for z in self.config]
+
+    def __exists_in_config(self, file, section, var_name):
+        """
+        search in config file in section to varname and return this
+        :param cfg:
+        :param section:
+        :param var_name:
+        :return:
+        """
+        try:
+            self.cfg.read(file)
+        except:
+            raise Exception("Can not read file:{0}".format(file))
+
+        sections = filter(lambda x: x == section, self.cfg.sections())
+        var_list = [self.__get_config_section_var(self.cfg, section, var_name) for z in sections]
+        return reduce(lambda x: x, var_list)
+
+    def __get_config_section_var(self, cfg, section, var):
+        try:
+            return cfg.get(section, var)
+        except:
+            return None
--- tests/test_configparser.py
+++ tests/test_configparser.py
@@ -13,7 +13,18 @@ def test_load():
     assert test_file_1 is not None
     assert cfgs is not None
 
+
 def test_load_empty_list():
     c = Config(files=('/etc/mysql/my123.cnf',))
     cfgs = c.load()
     assert len(cfgs) is 0
+
+
+def test_search_var():
+    test_file_1 =  os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_files/debian.cfg")
+    test_file_2 =  os.path.join(os.path.dirname(os.path.realpath(__file__)), "test_files/debian2.cfg")
+    c = Config(files=(test_file_1, '/etc/mysql/my123.cnf', test_file_2))
+    cfgs = c.load()
+
+    var = c.search('client', 'password')
+    assert var == ['meinpassword', 'meinpassword']
